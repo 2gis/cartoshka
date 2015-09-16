681aea6ed86e3dc6218ff403d472100a79a17819
Artem Tartakynov <artem.tad@gmail.com>
2015-09-17 00:30:13 +0600

    added some basic tests for tokenizer

--- src/test/java/com/github/tartakynov/cartoshka/tokenizers/TokenizerTest.java
+++ src/test/java/com/github/tartakynov/cartoshka/tokenizers/TokenizerTest.java
@@ -1,48 +1,105 @@
 package com.github.tartakynov.cartoshka.tokenizers;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class TokenizerTest {
-    Tokenizer tokenizer = null;
-
     @Test
-    public void testTokenizer() {
-        Assert.assertEquals(tokenizer.next().getType(), TokenType.VARIABLE);
-        Assert.assertEquals(tokenizer.current().getText(), "hello");
+    public void testQuotedText() {
+        Tokenizer tokenizer = createTokenizer("\'abraca\' \"dabra\"");
+        Assert.assertEquals(TokenType.STRING_LITERAL, tokenizer.next().getType());
+        Assert.assertEquals("abraca", tokenizer.current().getText());
 
-        Assert.assertEquals(tokenizer.next().getType(), TokenType.HASHNAME);
-        Assert.assertEquals(tokenizer.current().getText(), "world");
+        Assert.assertEquals(TokenType.STRING_LITERAL, tokenizer.next().getType());
+        Assert.assertEquals("dabra", tokenizer.current().getText());
 
-        Assert.assertEquals(tokenizer.next().getType(), TokenType.NUMBER_LITERAL);
-        Assert.assertEquals(tokenizer.current().getText(), "1.2");
-
-        Assert.assertEquals(tokenizer.next().getType(), TokenType.NUMBER_LITERAL);
-        Assert.assertEquals(tokenizer.current().getText(), "1.2e+5");
-
-        Assert.assertEquals(tokenizer.next().getType(), TokenType.STRING_LITERAL);
-        Assert.assertEquals(tokenizer.current().getText(), "quoted");
-
-        Assert.assertEquals(tokenizer.next().getType(), TokenType.IDENTIFIER);
-        Assert.assertEquals(tokenizer.current().getText(), "Id3nti-er");
-
-        Assert.assertEquals(tokenizer.next().getType(), TokenType.MUL);
-        Assert.assertEquals(tokenizer.current().getText(), "");
-
-        Assert.assertEquals(tokenizer.next().getType(), TokenType.IDENTIFIER);
-        Assert.assertEquals(tokenizer.current().getText(), "x");
-
-        Assert.assertEquals(tokenizer.next().getType(), TokenType.IDENTIFIER);
-        Assert.assertEquals(tokenizer.current().getText(), "zoom-x");
+        Assert.assertEquals(TokenType.EOS, tokenizer.next.getType());
     }
 
-    @Before
-    public void init() {
-        tokenizer = new Tokenizer() {
-            private char[] source = ("@hello #world 1.2 1.2e+5 'quoted'\n" +
-                    "/* comment */ <!-- html comment --> Id3nti-er*" +
-                    "\n// line comment\nx zoom-x").toCharArray();
+    @Test
+    public void testNumbers() {
+        Tokenizer tokenizer = createTokenizer("3.1415926535897932384 26e-2");
+        Assert.assertEquals(TokenType.NUMBER_LITERAL, tokenizer.next().getType());
+        Assert.assertEquals("3.1415926535897932384", tokenizer.current().getText());
+
+        Assert.assertEquals(TokenType.NUMBER_LITERAL, tokenizer.next().getType());
+        Assert.assertEquals("26e-2", tokenizer.current().getText());
+
+        Assert.assertEquals(TokenType.EOS, tokenizer.next.getType());
+    }
+
+    @Test
+    public void testKeywords() {
+        Tokenizer tokenizer = createTokenizer("true false null Map zoom");
+        Assert.assertEquals(TokenType.TRUE_LITERAL, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.FALSE_LITERAL, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.NULL_LITERAL, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.MAP_KEYWORD, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.ZOOM_KEYWORD, tokenizer.next().getType());
+
+        Assert.assertEquals(TokenType.EOS, tokenizer.next.getType());
+    }
+
+    @Test
+    public void testHashNames() {
+        Tokenizer tokenizer = createTokenizer("#first-1 #second-2");
+        Assert.assertEquals(TokenType.HASHNAME, tokenizer.next().getType());
+        Assert.assertEquals("first-1", tokenizer.current().getText());
+
+        Assert.assertEquals(TokenType.HASHNAME, tokenizer.next().getType());
+        Assert.assertEquals("second-2", tokenizer.current().getText());
+
+        Assert.assertEquals(TokenType.EOS, tokenizer.next.getType());
+    }
+
+    @Test
+    public void testVariables() {
+        Tokenizer tokenizer = createTokenizer("@first-1 @second-2");
+        Assert.assertEquals(TokenType.VARIABLE, tokenizer.next().getType());
+        Assert.assertEquals("first-1", tokenizer.current().getText());
+
+        Assert.assertEquals(TokenType.VARIABLE, tokenizer.next().getType());
+        Assert.assertEquals("second-2", tokenizer.current().getText());
+
+        Assert.assertEquals(TokenType.EOS, tokenizer.next.getType());
+    }
+
+    @Test
+    public void testSkippingWhiteSpaces() {
+        Tokenizer tokenizer = createTokenizer("     /* multiline\n" +
+                "comment\r\n" +
+                "*/ // sinle line comment\n" +
+                "<!-- HTML comment\n" +
+                "multiline --> 1");
+        Assert.assertEquals(TokenType.NUMBER_LITERAL, tokenizer.next().getType());
+        Assert.assertEquals("1", tokenizer.current().getText());
+
+        Assert.assertEquals(TokenType.EOS, tokenizer.next.getType());
+    }
+
+    @Test
+    public void testCharacters() {
+        Tokenizer tokenizer = createTokenizer("+-*/% >= <= > < =:;,");
+        Assert.assertEquals(TokenType.ADD, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.SUB, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.MUL, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.DIV, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.MOD, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.GTE, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.LTE, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.GT, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.LT, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.EQ, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.COLON, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.SEMICOLON, tokenizer.next().getType());
+        Assert.assertEquals(TokenType.COMMA, tokenizer.next().getType());
+
+        Assert.assertEquals(TokenType.EOS, tokenizer.next.getType());
+    }
+
+    private static Tokenizer createTokenizer(final String str) {
+        return new Tokenizer() {
+            private char[] source = str.toCharArray();
             private int position = -1;
 
             public Tokenizer init() {
